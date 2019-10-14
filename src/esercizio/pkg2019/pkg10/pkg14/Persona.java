d3d247f8bb5765e093ab8820638a03b20f17de6c
LAB-INFORM-1 PC16 <LAB-INFORM-1 PC16@DESKTOP-0MU5PBT>
2019-10-14 12:34:44 +0200

    commit "Dichiarazione Costruttore"

--- src/esercizio/pkg2019/pkg10/pkg14/Persona.java
+++ src/esercizio/pkg2019/pkg10/pkg14/Persona.java
@@ -10,9 +10,17 @@ package esercizio.pkg2019.pkg10.pkg14;
  * @author LAB-INFORM-1 PC16
  */
 public class Persona {
-    public String nome;
-    public String cognome;
-    public String titolo;
-    public String professione;
+    private String nome;
+    private String cognome;
+    private String titolo;
+    private String professione;
+    
+    public Persona(String nome,String cognome,String titolo,String professione)
+    {
+       this.nome=nome;
+       this.cognome=cognome;
+       this.titolo=titolo;
+       this.professione=titolo;
+    }
     
 }
