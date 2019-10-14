5c0e95c741d7a54b9248b4e9a6a73fe0fdc67ce3
LAB-INFORM-1 PC16 <LAB-INFORM-1 PC16@DESKTOP-0MU5PBT>
2019-10-14 12:29:15 +0200

    commit "Dichiarazione Variabili"

--- src/esercizio/pkg2019/pkg10/pkg14/Persona.java
+++ src/esercizio/pkg2019/pkg10/pkg14/Persona.java
@@ -9,15 +9,10 @@ package esercizio.pkg2019.pkg10.pkg14;
  *
  * @author LAB-INFORM-1 PC16
  */
-public class Esercizio20191014 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-               
-    }
+public class Persona {
+    public String nome;
+    public String cognome;
+    public String titolo;
+    public String professione;
     
 }
